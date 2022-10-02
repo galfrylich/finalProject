fc6ae682c6f916fab376ad872c44c281febe9fb4
galfrylich <galfrylich@gmail.com>
2022-10-02 12:37:06 +0300

    fix app

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask,render_template
 from config_db import *
+from get_csv_files import *
+
 
 
 app = Flask(__name__)
 
+@app.before_first_request
+def init():
+    conn = init_db()
+    file_handler(conn)
 
 @app.route("/")
 def base():
@@ -14,9 +20,6 @@ def base():
 def index():
     return render_template('index.html')    
 
-@app.before_first_request
-def init():
-    init_db()
 
 @app.route("/show_table")
 def show_students():
@@ -25,7 +28,20 @@ def show_students():
     cursor.execute("USE attendance_DB")
     cursor.execute("SELECT * FROM attendance_list")
     all_students = cursor.fetchall()
+    print(all_students)
     return render_template('show_students.html', students=all_students)
+
+
+def file_handler(connection):
+    csv_files = file_transfer()
+    for file_name in csv_files:
+        file_path = '/app/csv_files/{}'.format(file_name)
+        file_name = file_name.split('-')[1]
+        file_id = file_name.split('.')[0]
+        insert_to_db(file_path,file_id,connection)
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')   
