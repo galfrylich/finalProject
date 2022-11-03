eb5bac044855691c8e6bafbb27ecb0676aabd376
galfrylich <galfrylich@gmail.com>
2022-11-03 16:40:49 +0200

    fix bug

--- deploy-project.sh
+++ deploy-project.sh
@@ -6,7 +6,7 @@ HOME_DIR=/home/ec2-user
 SECRET_KEY=${HOME_DIR}/key
 PIPLINE_WORKSPACE=/var/lib/jenkins/workspace/final-project
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 1 ]; then
     echo "please enter one argument"
     exit 1
 fi
@@ -15,7 +15,7 @@ fi
 
 machine_name=$1
 
-
+echo $#
 ssh -i $SECRET_KEY -o StrictHostKeyChecking=no $machine_name mkdir -p $HOME_DIR/final-project
 scp -i $SECRET_KEY $PIPLINE_WORKSPACE/docker-compose.yml $machine_name:$HOME_DIR/final-project
 
