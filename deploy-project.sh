d38d00fdae5a2b7959aa8a8c5c78eb1ff8552e2c
galfrylich <galfrylich@gmail.com>
2022-11-06 12:48:09 +0200

    remove -v

--- deploy-project.sh
+++ deploy-project.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 
-###########THIS IS A BASH SCRIPT TO DEPLOY MY WEB APP######
+###########THIS IS A BASH SCRIPT TO DEPLOY MY WEB APP
+### accepts one argument, the host name macine.######
 
 #VARS#
 HOME_DIR=/home/ec2-user
 PIPLINE_WORKSPACE=/var/lib/jenkins/workspace/final-project
+machine_name=$1
 
 if [ $# -lt 1 ]; then
     echo "please enter one argument"
     exit 1
 fi
 
-    
-   
-
-machine_name=$1
 
 ssh -v -o StrictHostKeyChecking=no -l ec2-user $machine_name mkdir -p $HOME_DIR/final-project
-scp -v $PIPLINE_WORKSPACE/docker-compose.yml ec2-user@$machine_name:$HOME_DIR/final-project
-ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name cd $HOME_DIR/final-project  && docker system prune -a --volumes -f
+scp $PIPLINE_WORKSPACE/docker-compose.yml ec2-user@$machine_name:$HOME_DIR/final-project
+
+ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name cd $HOME_DIR/final-project && docker system prune -a --volumes -f
 ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name cd $HOME_DIR/final-project && docker-compose up -d
 
