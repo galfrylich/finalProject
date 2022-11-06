d39d3cc63dab5901afa4090051bb2ec17d1d9954
galfrylich <galfrylich@gmail.com>
2022-11-06 21:07:30 +0200

    remove buile line

--- deploy-project.sh
+++ deploy-project.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
+### USAGE ###
 ###########THIS IS A BASH SCRIPT TO DEPLOY MY WEB APP
-### accepts one argument, the host name macine.######
+### Accepts one argument, the host name macine.######
 
 #VARS#
 HOME_DIR=/home/ec2-user
@@ -14,10 +14,20 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-
-
+# cnnecting to machine create new dir
 ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name mkdir -p $HOME_DIR/final-project
+# copy docker compose to machine
 scp $PIPLINE_WORKSPACE/docker-compose.yml ec2-user@$machine_name:$HOME_DIR/final-project
+# remove all images and containers
+ssh ec2-user@test "cd /home/ec2-user/final-project; docker system prune -a --volumes -f"
+# run docker compose up
 ssh ec2-user@test "cd /home/ec2-user/final-project; docker-compose up -d"
-#ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name cd $HOME_DIR/final-project && docker-compose up -d
+
+##if [ $machine_name == "test"]; then 
+   # scp $PIPLINE_WORKSPACE/tests ec2-user@$machine_name:$HOME_DIR/final-project
+   # ssh ec2-user@test "cd /home/ec2-user/final-project/tests; ./test.sh " 
+
+
+
+
 
