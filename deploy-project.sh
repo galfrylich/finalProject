242e1ef7315631f3d2d49a46a9a5eaef067f63ac
galfrylich <galfrylich@gmail.com>
2022-11-06 08:59:03 +0200

    fix ssh to test macine

--- deploy-project.sh
+++ deploy-project.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-        #THIS IS A BASH SCRIPT TO DEPLOY MY WEB APP
 
-#VARS
+
+###########THIS IS A BASH SCRIPT TO DEPLOY MY WEB APP######
+
+#VARS#
 HOME_DIR=/home/ec2-user
-SECRET_KEY=${HOME_DIR}/key
 PIPLINE_WORKSPACE=/var/lib/jenkins/workspace/final-project
 
 if [ $# -lt 1 ]; then
     echo "please enter one argument"
     exit 1
 fi
+
     
    
 
 machine_name=$1
 
-echo $#
-echo $SECRET_KEY
+
 ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name -p $HOME_DIR/final-project
 scp  $PIPLINE_WORKSPACE/docker-compose.yml $machine_name:$HOME_DIR/final-project
-docker system prune -a --volumes -f
-docker-compose up -d
+ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name cd $HOME_DIR/finale-project docker system prune -a --volumes -f
+ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name cd $HOME_DIR/finale-project docker-compose up -d
+
 
