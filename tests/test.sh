0518388b515e20edf8562cae4a1dbf496378ffc1
galfrylich <galfrylich@gmail.com>
2022-11-07 14:11:06 +0200

    add fails

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex 
 
-URL=$(curl  'http://checkip.amazonaws.com')
-STSTUS_CODE=$(curl -Is $URL:5000 | head -n 1)
-if [ $STSTUS_CODE == "HTTP/1.1 200 OK" ]; then
+
+STSTUS_CODE=$(curl -Is 127.0.0.1:5000 | head -n 1)
+if [ $STATUS_CODE == "HTTP/1.1 200 OK" ]; then
     echo "App is running"
 else 
     echo "response code:" + $STSTUS_CODE + ", exit pipline"
     exit 1 
-
+fi 
