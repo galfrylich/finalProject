2918ee788cb5810e117e927382cfd427c6a954ba
galfrylich <galfrylich@gmail.com>
2022-11-07 08:40:18 +0200

    change machine name

--- deploy-project.sh
+++ deploy-project.sh
@@ -7,6 +7,7 @@
 #VARS#
 HOME_DIR=/home/ec2-user
 PIPLINE_WORKSPACE=/var/lib/jenkins/workspace/final-project
+DIR_NAME=final-project
 machine_name=$1
 
 if [ $# -lt 1 ]; then
@@ -15,13 +16,13 @@ if [ $# -lt 1 ]; then
 fi
 
 # cnnecting to machine create new dir
-ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name mkdir -p $HOME_DIR/final-project
+ssh -o StrictHostKeyChecking=no -l ec2-user $machine_name mkdir -p $HOME_DIR/$DIR_NAME
 # copy docker compose to machine
-scp $PIPLINE_WORKSPACE/docker-compose.yml ec2-user@$machine_name:$HOME_DIR/final-project
+scp $PIPLINE_WORKSPACE/docker-compose.yml ec2-user@$machine_name:$HOME_DIR/$DIR_NAME
 # remove all images and containers
-ssh ec2-user@test "cd /home/ec2-user/final-project; docker system prune -a --volumes -f"
+ssh ec2-user@$machine_name "cd /home/ec2-user/$DIR_NAME; docker system prune -a --volumes -f"
 # run docker compose up
-ssh ec2-user@test "cd /home/ec2-user/final-project; docker-compose up -d"
+ssh ec2-user@$machine_name "cd /home/ec2-user/$DIR_NAME; docker-compose up -d"
 
 ##if [ $machine_name == "test"]; then 
    # scp $PIPLINE_WORKSPACE/tests ec2-user@$machine_name:$HOME_DIR/final-project
