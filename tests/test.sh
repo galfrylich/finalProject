5dc90bf3262f47b2df797ba62bf97200df746f90
galfrylich <galfrylich@gmail.com>
2022-11-10 11:32:55 +0200

    update test

--- tests/test.sh
+++ tests/test.sh
@@ -2,10 +2,11 @@
 
 
 STATUS_CODE=$(curl -Is 127.0.0.1:5000 | head -n 1)
-echo $STATUS_CODE
+
 if [ "$STATUS_CODE"="HTTP/1.1 200 OK" ]; then
     echo "App is running"
 else 
     echo "response code: $STATUS_CODE , exit pipline"
     exit 1 
 fi 
+
